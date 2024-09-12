0cef9004d12368488160578597721c702c578550
Kshitij <kshitij234srivastava@gmail.com>
2024-09-13 02:53:00 +0530

    Initial commit of the server projec88

--- database/connection.js
+++ database/connection.js
@@ -1,17 +1,14 @@
 const mongoose = require('mongoose');
+
 async function connectToDatabase() {
     try {
-        main().catch(err => console.log(err));
-
-        async function main() {
-        await mongoose.connect('mongodb://127.0.0.1:27017/NextWatch');
-
-            // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-        }
-    }
-    catch(error)
-    {
-        console.log(error);
+        await mongoose.connect('mongodb+srv://kpsrivastava:Kshitij@26@kpsrivastava.qbhiqum.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to MongoDB Atlas');
+    } catch (error) {
+        console.error('Error connecting to MongoDB Atlas:', error);
     }
 }
 
